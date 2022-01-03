5e3b85f0c768b909e1c63702e7913caacc3a7dd8
webster <wbian618@gmail.com>
2022-01-03 18:09:17 -0500

    CRUD actions for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,30 @@ class PostsController < ApplicationController
         render json: post
     end
 
+    def show
+        post = Post.find(params[:id])
+        render json: post
+    end
+
+    def update
+        post = Post.find(params[:id])
+        post.update(update_params)
+        render json: post
+    end
+
+    def destroy
+        post = Post.find(params[:id])
+        post.destroy
+        render json: {}
+    end
+
     private
+
     def post_params
         params.permit(:title, :body, :photos, :user_id)
+    end
+
+    def update_params
+        params.permit(:title, :body, :photos)
     end
 end
