c7bd57bf2e9068ad80a205e69c8e7212d1a8d7c9
dylanjenks <dylan.jenks92@gmail.com>
2022-01-05 15:27:05 -0500

    Login backend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_03_153951) do
+ActiveRecord::Schema.define(version: 2022_01_05_195749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,9 +46,9 @@ ActiveRecord::Schema.define(version: 2022_01_03_153951) do
     t.string "name"
     t.string "username"
     t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "posts"
